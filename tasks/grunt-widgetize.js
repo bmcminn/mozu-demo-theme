a4fce013b39cf9d6b5a92899c185b2748c9f4095
bmcminn <labs@gbox.name>
2015-03-13 18:25:57 -0500

    staging updates to widget config process

---
+++ tasks/grunt-widgetize.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(grunt) {
+
+//
+//
+//
+
+
+  grunt.registerMultiTask('widgetize', 'Add/remove widget configs and assets from a central widget collection directory.', function() {
+
+    var pkg             = grunt.file.readJSON('package.json'),
+        theme           = grunt.file.readJSON('theme.json'),
+
+
+
+  });
+};
