03f93f319753d6f8ac728422d4dcf846957ea3ee
bmcminn <labs@gbox.name>
2015-08-27 13:56:51 -0500

    fixing grunt configs directory

---
+++ grunt/aliases.js
@@ -0,0 +1,40 @@
+// grunt/tasks/default.js
+
+module.exports = function(grunt) {
+
+  grunt.registerTask('default', [
+    'widgetize'
+  , 'jsonlint'
+  , 'jshint'
+  , 'setver:build'
+  , 'compress'
+  ]);
+
+  grunt.registerTask('build', [
+    'jsonlint'
+  , 'jshint'
+  , 'checkreferences'
+  , 'zubat'
+  , 'setver:release'
+  , 'compress'
+  ]);
+
+  grunt.registerTask('email', [
+    'juice'
+  , 'strainer'
+  ]);
+
+  grunt.registerTask('push', [
+    'jsonlint'
+  , 'jshint'
+  , 'mozutheme:check'
+  , 'mozutheme:quickcompile'
+  , 'mozutheme:buildver'
+  , 'mozusync:upload'
+  ]);
+
+  grunt.registerTask('sync', [
+    'watch:sync'
+  ]);
+
+};
