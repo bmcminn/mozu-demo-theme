a87726db1b1da09a94c4945955a3edc2f060087d
bmcminn <labs@gbox.name>
2016-02-04 15:29:05 -0600

    added slideshow-bob and slideshow-mel widget modules

---
+++ .components/widgets/slideshow-bob/slideshow-bob.js
@@ -0,0 +1,21 @@
+define([
+  'modules/jquery-mozu'
+, 'vendor/slick'
+], function($) {
+
+    var temp
+      , regex = {
+        }
+      , $slideshows   = $('[data-mz-widget="slideshow-bob"]')
+      , $slides       = $('[data-mz-widget="slideshow-mel"]')
+      ;
+
+
+    console.debug('slideshows', $slideshows);
+    console.debug('slides',     $slides);
+
+    // need to finish adding functionality here
+
+
+    // $slideshow.slick();
+});
