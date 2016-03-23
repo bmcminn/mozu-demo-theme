2f46fa13a4bc2bafa95c7a590226d7314b6a9f75
Brandtley <labs@gbox.name>
2016-03-23 14:15:59 -0500

    updating three-sixty widget with possible process update to support mobile/touch devices

--- .theme/widgets/three-sixty/three-sixty.js
+++ .theme/widgets/three-sixty/three-sixty.js
@@ -28,7 +28,8 @@ function(_, $) {
       , model = JSON.parse($this.dataset.mzConfig)
       , image = model.config
       , frame
-      , i = 0
+      , x = 0
+      , y = 0
       ;
 
 
@@ -39,21 +40,24 @@ function(_, $) {
     model.$images = [];
 
 
-    for (i=0; i<image.frameCount; i+=1) {
+    for (x=0; x<image.frameCount; x+=1) {
 
+      // set the crop parameter offsets
       image.frame = [
-        i * image.frameWidth
-      , 0
-      , (i+1) * image.frameWidth
-      , image.height
+        x * image.frameWidth      // x1
+      , y                         // y1
+      , (x+1) * image.frameWidth  // x2
+      , image.height              // y2
       ].join(',');
 
+      // append our new image reference to the image array list
       model.$images.push([
         image.URL.imageUrl
       , '?quality=', image.quality
       , '&crop=', image.frame
       ].join(''));
 
+      // clean up the frame property
       delete(image.frame);
     }
 
@@ -93,3 +97,90 @@ function(_, $) {
 
 
 });
+
+
+
+/// TESTING AUTOMATIC CAPTURING OF FRAME HEIGHT/WIDTHS
+// var model = {
+//     x: 0,
+//     y: 0
+//   },
+
+//   image = {
+//     numFrames: 90,
+//     width: 24560,
+//     height: 4800,
+//     // frameWidth: 1228,
+//     // frameHeight: 801,
+//     xframes: 20,
+//     yframes: 8,
+//     MAX_SIZE: 30000
+//   },
+
+//   frames = [],
+//   RADIX = 10
+//   ;
+
+
+
+// /**
+//  * [sanityCheck description]
+//  * @return {[type]} [description]
+//  */
+// var sanityCheck = function() {
+//   if (image.width < image.MAX_SIZE && image.height < image.MAX_SIZE) {
+//     return true;
+
+//   } else {
+//     console.error('The image can be no larger than', image.MAX_SIZE, 'pixels in either dimension', model.image);
+//     return false;
+//   }
+// }
+
+
+
+// /**
+//  * [getFrames description]
+//  * @return {[type]} [description]
+//  */
+// var getFrames = function() {
+
+//   image.frameWidth = image.width / image.xframes;
+//   image.frameHeight = image.height / image.yframes;
+
+//   // check if the resulting number of x/y frames is not a whole number
+//   if (parseInt(image.xframes, RADIX).toString().length < image.xframes.toString().length) {
+//     console.warn('The given frame width', image.frameWidth, 'yields an uneven number of frames.', image.xframes);
+//   }
+
+//   if (parseInt(image.yframes, RADIX).toString().length < image.yframes.toString().length) {
+//     console.warn('The given frame height', image.frameHeight, 'yields an uneven number of frames.', image.yframes);
+//   }
+
+
+//   console.log(image);
+
+// }
+
+
+
+
+
+
+// /**
+//  * [init description]
+//  * @return {[type]} [description]
+//  */
+// var init = function() {
+
+//   if (!sanityCheck()) {
+//     return;
+//   }
+
+//   getFrames();
+
+// }
+
+
+// // get the party started
+// init();
